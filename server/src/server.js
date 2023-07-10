42e96dbeda74ae3db637d8d18e8b3ddf5d179baf
Marcelo Mechi <marcedumechi@gmail.com>
2023-07-10 20:42:11 -0300

    feat: adds mongodb and mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const cluster = require('cluster')
+const mongoose = require('mongoose')
 cluster.schedulingPolicy = cluster.SCHED_RR
 
 const app = require('./app');
@@ -9,7 +10,18 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+const MONGO_URL = 'mongodb+srv://marcedumechi:obFbfvD4vaJWr9aM@cluster0.yyjiygm.mongodb.net/nasa?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection has been established!')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.error(err)
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
